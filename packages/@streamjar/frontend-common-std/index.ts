ad6c565b67a8fc11ba67ffce16dec7ac8807e88f
Luke Thompson <luke@streamjar.tv>
2018-10-17 01:04:43 +0100

    feat(std-lib): Implement standard http lib using fetch api

---
+++ packages/@streamjar/frontend-common-std/index.ts
@@ -0,0 +1,82 @@
+import { HttpService } from '@streamjar/frontend-common-core';
+import { Observable, Subject } from 'rxjs';
+
+let apiAdapter: JarService;
+
+export class InvalidResponseError extends Error {
+	public statusCode: number;
+
+	constructor(response: Response) {
+		super(`Error ${response.status}`);
+		this.statusCode = response.status;
+		Object.setPrototypeOf(this, InvalidResponseError.prototype);
+	}
+}
+
+export class JarService extends HttpService {
+	private getHeadersFn: (url: string) =>  { [key: string]: string };
+
+	public get<T>(uri: string): Observable<T> {
+		return this.request<T>('GET', uri);
+	}
+
+	public post<T>(uri: string, body: any): Observable<T> {
+		return this.request<T>('POST', uri, body);
+	}
+
+	public patch<T>(uri: string, body: any): Observable<T> {
+		return this.request<T>('PATCH', uri, body);
+	}
+
+	public put<T>(uri: string, body: any): Observable<T> {
+		return this.request<T>('PUT', uri, body);
+	}
+
+	public delete<T>(uri: string): Observable<T> {
+		return this.request<T>('DELETE', uri);
+	}
+
+	private request<T>(method: string, url: string, body?: string): Observable<T> {
+		const obs = new Subject<T>();
+
+		let bodyValue;
+		const headers: { [key: string]: string } = this.getHeadersFn ? this.getHeadersFn(url) : this.getHeaders(url);
+
+		if (body) {
+			bodyValue = JSON.stringify(body);
+			headers['Content-Type'] = 'application/json';
+		}
+
+		fetch(this.buildUrl(url), {
+			body: bodyValue,
+			headers: headers,
+			method: method.toUpperCase(),
+		}).then((value) => {
+			if (!value.ok) {
+				throw new InvalidResponseError(value);
+			}
+
+			if (value.status === 204) {
+				return null;
+			}
+
+			return value.json();
+		}).then(value => {
+			obs.next(value);
+			obs.complete();
+		}).catch((err) => {
+			obs.error(err);
+			obs.complete();
+		});
+
+		return obs;
+	}
+}
+
+export const getApi = <T>(item: new (a: HttpService) => T): T => {
+	return new (<any>item)(apiAdapter);
+}
+
+export const setApiAdapter = (adapter: JarService): void => {
+	apiAdapter = adapter;
+}
