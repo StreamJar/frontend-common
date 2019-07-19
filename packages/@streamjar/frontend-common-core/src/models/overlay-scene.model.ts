1397aeb3cf3638194f2c9195072912e3a0eb1a33
Luke Thompson <luke@streamjar.tv>
2019-07-19 17:39:11 +0100

    feat(overlay): Expose soft deleting a scene

--- packages/@streamjar/frontend-common-core/src/models/overlay-scene.model.ts
+++ packages/@streamjar/frontend-common-core/src/models/overlay-scene.model.ts
@@ -1,4 +1,6 @@
 import { BaseModel, Dated, Identifiable } from './base';
+import { IChannel } from './channel.model';
+import { Observable } from 'rxjs';
 
 export interface IOverlayScene extends Identifiable, Dated {
 	color: 'transparent' | 'blue' | 'green';
@@ -10,8 +12,11 @@ export interface IOverlayScene extends Identifiable, Dated {
 }
 
 export class OverlayScene extends BaseModel<IOverlayScene> {
-	public writable: string[] = ['color', 'width', 'height', 'name', 'sceneId']; // sceneId is a weird quirk of the api
+	public writable: string[] = ['color', 'width', 'height', 'name', 'sceneId', 'deleted']; // sceneId is a weird quirk of the api
 
 	public endpoint = 'overlay/scenes';
 
+	public update(channel: IChannel, scene: IOverlayScene & { deleted?: boolean }): Observable<IOverlayScene> {
+		return super.update(channel, scene);
+	}
 }
