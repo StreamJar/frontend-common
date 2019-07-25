207cb8448d89b306f9e60afac677b9e99a9e15ce
Luke Thompson <luke@streamjar.tv>
2019-07-25 02:41:41 +0100

    feat(message): Use updated endpoint

--- packages/@streamjar/frontend-common-core/src/models/message.model.ts
+++ packages/@streamjar/frontend-common-core/src/models/message.model.ts
@@ -7,10 +7,13 @@ export interface IMessage extends Dated {
 	content?: string;
 	platforms?: string[];
 	recipient?: boolean;
+	level?: string[];
+	cost?: number;
+	enabled?: boolean;
 }
 
 export class Message extends BaseModel<IMessage> {
-	public writable: string[] = ['name', 'content', 'platforms', 'recipient'];
+	public writable: string[] = ['name', 'content', 'platforms', 'recipient', 'enabled', 'cost', 'level'];
 	public endpoint = 'messages';
 
 	constructor(protected jar: HttpService) {
