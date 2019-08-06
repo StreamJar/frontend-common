ff2cd830e39321519855a98ae66bfdaba908550e
Luke Thompson <luke@streamjar.tv>
2019-08-07 00:34:12 +0100

    feat(payments): Support config url

--- packages/@streamjar/frontend-common-core/src/models/tip.model.ts
+++ packages/@streamjar/frontend-common-core/src/models/tip.model.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { HttpService } from '../services/http.service';
 import { BaseModel, Dated } from './base';
 import { IChannel } from './channel.model';
+import { omit } from '../utils';
 
 export interface ITip extends Dated {
 	id: number;
@@ -34,6 +35,13 @@ export interface ITipAudio {
 	hash: string;
 }
 
+export interface IPaymentGatewayConfig {
+	currency: string;
+	enabled: boolean; 
+	minAmount: number;
+	maxAmount: number;
+}
+
 export class Tip extends BaseModel<ITip> {
 	public static writable: string[] = [];
 	public endpoint = 'tips';
@@ -46,8 +54,12 @@ export class Tip extends BaseModel<ITip> {
 		return this.jar.get<ITip[]>(`channels/${channel.id}/tips/summary?sort=${filter}`);
 	}
 
+	public getConfig(channel: IChannel, gateway: string): Observable<IPaymentGatewayConfig> {
+		return this.jar.get<IPaymentGatewayConfig>(`channels/${channel.id}/tip/${gateway}`);
+	}
+
 	public createExternalTip(channel: IChannel, tip: IExternalTip): Observable<{ redirect: string }> {
-		return this.jar.post<{ redirect: string }>(`channels/${channel.id}/tip`, tip);
+		return this.jar.post<{ redirect: string }>(`channels/${channel.id}/tip/${tip.method}`, omit(tip, 'method'));
 	}
 
 	public uploadAudio(channel: IChannel, file: any) {
